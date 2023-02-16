1df78dd7cd40e01d9554a5abb08b7f1091d766d7
DavidMacharia62 <davidmacharia1@gmail.com>
2023-02-16 16:21:04 +0300

    Corrections to program 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 /* A c program that outputs '"Programming is like building a multilingual puzzle' */
 /* Created by David Macharia for the alx Software Engineering course */
+/* main - returns 0 if there is no error */
 
 #include <stdio.h>
 
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
