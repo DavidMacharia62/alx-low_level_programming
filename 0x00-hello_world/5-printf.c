8ab1264746dd35e4d2abe15f2eba8b0d862af426
DavidMacharia62 <davidmacharia1@gmail.com>
2023-02-16 18:51:18 +0300

    Creation of the program 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
