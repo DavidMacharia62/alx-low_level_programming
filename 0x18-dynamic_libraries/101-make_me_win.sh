665b329a9c2c46af10943b85a2e88d919e6cd4bd
DavidMacharia62 <davidmacharia1@gmail.com>
2023-05-22 19:41:46 +0300

    Task 101: 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp/ https://github.com/DavidMachari62/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mymole.so
-export LD_PRELOAD="/tmp/mymole.so"
+wget -qP /tmp/ https://github.com/DavidMachari62/alx-low_level_programming/raw/master/0x18-dynamic_libraries/new_mole.so
+export LD_PRELOAD="/tmp/new_mole.so"
