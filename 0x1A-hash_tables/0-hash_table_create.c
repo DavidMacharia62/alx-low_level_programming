7cb493bf77034827e6b5075b468c0aabc7f382bc
DavidMacharia62 <davidmacharia1@gmail.com>
2023-06-09 13:28:46 +0300

    Task 0: 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
 	unsigned long int i;
-
-    /* Allocate memory for the hash table structure */
+	
+	/* Allocate memory for the hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return NULL;
